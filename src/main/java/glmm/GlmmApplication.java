a4f889c802593c47ffabf5ac10a750818a3fe763
Atari Oberon <atari-oberon@mutsch-laptop.atari-oberon>
2025-01-08 11:01:37 +0700

    created spring-boot application

---
+++ src/main/java/glmm/GlmmApplication.java
@@ -0,0 +1,13 @@
+package glmm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlmmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlmmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/glmm/GlmmApplicationTests.java
@@ -0,0 +1,13 @@
+package glmm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlmmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
